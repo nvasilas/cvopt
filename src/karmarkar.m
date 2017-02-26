01bb48b99e86e8d4941d9fcfc346a5b90059e26a
Nikos Vasilas <nikos4807@gmail.com>
2017-02-26 16:22:06 +0200

    Update karmarkar.m (help included)

--- src/karmarkar.m
+++ src/karmarkar.m
@@ -1,12 +1,41 @@
-function [x_opt, f_opt] = karmarkar()
-    c = [1; -3; 3];
-    A = [1, -3, 2];
-    p = struct('a', 0.25, 'q', 100, 'n', size(A, 2),...
+function [x_opt, f_opt, iter] = karmarkar(c, A, q, a)
+    % Karmarkar's algorith
+    % x = karmarkar(___) attempts to solve Karmarkar's restricted problem, that is,
+    % a linear programming problem in Karmarkar's canonical form which satisfies
+    % the four assumptions, as stated by Karmarkar in his original paper. The
+    % form of the problem is as follows:
+    % min c'*x subject to: A*x = 0, sum_{i=1}^n x_i = 1, x >=0
+    % The inputs of this function is
+    % the vector c, the coefficient of the objective function,
+    % the matrix A, the matrix coefficient of the constraints,
+    % the scalar q > 0, the termination parameter (optional),
+    % the scalar 0 < a < 1, the step size (optional).
+    % The output of x_opt = karmarkar(___) is the optimal value x^*.
+    % The outputs of [x_opt, f_opt] = karmarkar(___) are as above plus
+    % the optimal value of the objective f^* (expected to be zero).
+    % The outputs of [x_opt, f_opt, iter] = karmarkar(___) are as above plus
+    % the number of iterations required to converge to optimal solution.
+    % Usage example. Define the problem
+    % c = [1;-3; 3];
+    % A = [1, -3, 2];
+    % and then call from MATLAB command line the function x = karmarkar(c, A).
+
+    if ~exist('q','var')
+	% third parameter does not exist, using default value
+	q = 100;
+    end
+    if ~exist('a','var')
+	% fourth parameter does not exist, using default value
+	a = 0.25;
+    end
+    p = struct('a', a, 'q', q, 'n', size(A, 2),...
 	'x_0', [1/size(A, 2); 1/size(A, 2); 1/size(A, 2)]);
-    [x_opt, f_opt] = algorithm(A, c, p);
+    [x_opt, f_opt, iter] = algorithm(A, c, p);
 end
 
-function [x_knew, f_opt] = algorithm(A, c, p)
+function [x_knew, f_opt, iter] = algorithm(A, c, p)
+    % This function implements Karmarkar's algorithm
+    iter = 0;
     x_knew = p.x_0;
     while c'*x_knew/(c'*p.x_0) > 2^(-p.q)
 	x_k = x_knew;
@@ -17,6 +46,7 @@ function [x_knew, f_opt] = algorithm(A, c, p)
 end
 
 function x_knew = psi_map(A, c, x_k, p)
+    % This function finds the next point
     e = ones(p.n, 1);
     D_k = diag(x_k);
     B_k = [A*D_k; e'];
